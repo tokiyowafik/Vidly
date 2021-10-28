58d623e2d37001c26a47002f83eba54a806c3dc2
Robbi Wafik <robbiwafik31@gmail.com>
2021-10-29 06:17:05 +0700

    Add the conditional message.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -13,36 +13,41 @@ class Movies extends Component {
     }
 
     render() {
-        return (
-            <div className="container" style={{padding: 30}}>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Genre</th>
-                            <th>Stock</th>
-                            <th>Rate</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.movies.map(movie => 
+            const { length: count } = this.state.movies;
+            if (count === 0)
+                return <p>There are no movies in this database.</p>;
+        
+            return(
+                <React.Fragment>
+                    <p>Showing {this.state.movies.length} Movies in The Database</p>
+                    <table className="table">
+                        <thead>
                             <tr>
-                                <td>{movie.title}</td>
-                                <td>{movie.genre.name}</td>
-                                <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>
-                                <td><button
-                                    className="btn btn-danger" 
-                                    onClick={() => this.handleDelete(movie)}>
-                                        Delete
-                                    </button></td>
+                                <th>Title</th>
+                                <th>Genre</th>
+                                <th>Stock</th>
+                                <th>Rate</th>
+                                <th></th>
                             </tr>
-                        )}
-                    </tbody>
-                </table>
-            </div>
-        );
+                        </thead>
+                        <tbody>
+                            {this.state.movies.map(movie => 
+                                <tr>
+                                    <td>{movie.title}</td>
+                                    <td>{movie.genre.name}</td>
+                                    <td>{movie.numberInStock}</td>
+                                    <td>{movie.dailyRentalRate}</td>
+                                    <td><button
+                                        className="btn btn-danger" 
+                                        onClick={() => this.handleDelete(movie)}>
+                                            Delete
+                                        </button></td>
+                                </tr>
+                            )}
+                        </tbody>
+                    </table>
+                </React.Fragment>
+            );
     }
 }
 
