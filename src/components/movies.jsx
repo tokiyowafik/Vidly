5c5bb2be4d157654bbe39eb35e71a2894dd9d397
Robbi Wafik <robbiwafik31@gmail.com>
2021-10-29 03:55:29 +0700

    Render movies obj inside the table.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import {getMovies} from "../services/fakeMovieService"
+
 
 class Movies extends Component {
+    state = {
+        movies: getMovies(),
+    }
+
     render() {
-        return <h1>Hello </h1>;
+        return (
+            <div className="container" style={{padding: 30}}>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Genre</th>
+                            <th>Stock</th>
+                            <th>Rate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.movies.map(movie => 
+                            <tr>
+                                <td>{movie.title}</td>
+                                <td>{movie.genre.name}</td>
+                                <td>{movie.numberInStock}</td>
+                                <td>{movie.dailyRentalRate}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        );
     }
 }
 
